47e564b5a38e69a678e3e5544a82cf6e5c393ee5
BalazsJako <this.is.balazs.jako@gmail.com>
2018-05-10 17:37:36 +0100

    FIX: enable build with c++11 #9

--- TextEditor.cpp
+++ TextEditor.cpp
@@ -14,6 +14,18 @@ static const int cTextStart = 7;
 // - handle unicode/utf
 // - testing
 
+template<class InputIt1, class InputIt2, class BinaryPredicate>
+bool equal(InputIt1 first1, InputIt1 last1,
+	InputIt2 first2, BinaryPredicate p)
+{
+	for (; first1 != last1; ++first1, ++first2) {
+		if (!p(*first1, *first2)) {
+			return false;
+		}
+	}
+	return true;
+}
+
 TextEditor::TextEditor()
 	: mLineSpacing(0.0f)
 	, mUndoIndex(0)
