fb97f0a8b2ed986680ba383ffa8a77acd23899ea
Sam Hocevar <sam@hocevar.net>
2019-01-15 00:36:10 +0100

    Fix single line comment logic.
    It seems reasonable to check that langDef.mSingleLineComment is
    non-empty before trying to match it with the current line of code.

--- TextEditor.cpp
+++ TextEditor.cpp
@@ -1924,7 +1924,8 @@ void TextEditor::ColorizeInternal()
 						auto from = line.begin() + currentCoord.mColumn;
 						auto& startStr = mLanguageDefinition.mCommentStart;
 						auto& singleStartStr = mLanguageDefinition.mSingleLineComment;
-						if (currentCoord.mColumn + singleStartStr.size() <= line.size() &&
+						if (singleStartStr.size() > 0 &&
+							currentCoord.mColumn + singleStartStr.size() <= line.size() &&
 							equals(singleStartStr.begin(), singleStartStr.end(), from, from + singleStartStr.size(), pred))
 							withinSingleLineComment = true;
 						else if (!withinSingleLineComment && currentCoord.mColumn + startStr.size() <= line.size() &&
