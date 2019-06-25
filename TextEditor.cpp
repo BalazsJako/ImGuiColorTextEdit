BalazsJako/ImGuiColorTextEdit

commit de88d251843421477969e108cdd4ea0541d5adfb

Fix last unnecessary linefeed at the end of the GetText returning string

TextEditor.cpp
@@ -104,7 +104,9 @@ std::string TextEditor::GetText(const Coordinates & aStart, const Coordinates & 
 		{
 			istart = 0;
 			++lstart;
-			result += '\n';
+
+			if (lstart < lend)
+				result += '\n';
 		}
 	}
 

commit 0864414121f1ea4aaa164ecf3abf41dee7a98aa2

Fix GetText() regressions introduced by de88d25

TextEditor.cpp
@@ -104,9 +104,7 @@ std::string TextEditor::GetText(const Coordinates & aStart, const Coordinates & 
 		{
 			istart = 0;
 			++lstart;
-
-			if (lstart < lend)
-				result += '\n';
+			result += '\n';
 		}
 	}
 
@@ -2096,7 +2094,9 @@ const TextEditor::Palette & TextEditor::GetRetroBluePalette()
 
 std::string TextEditor::GetText() const
 {
-	return GetText(Coordinates(), Coordinates((int)mLines.size(), 0));
+	auto lastLine = (int)mLines.size() - 1;
+	auto lastLineLength = GetLineMaxColumn(lastLine);
+	return GetText(Coordinates(), Coordinates(lastLine, lastLineLength));
 }
 
 std::vector<std::string> TextEditor::GetTextLines() const