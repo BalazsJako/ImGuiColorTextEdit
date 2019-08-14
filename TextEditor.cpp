5239cea6c8f767f1e9b89a1aa669bba209c2a289
Marco Melorio <m.melorio@icloud.com>
2019-08-14 19:20:40 +0200

    Code improvement

--- TextEditor.cpp
+++ TextEditor.cpp
@@ -298,14 +298,8 @@ int TextEditor::InsertTextAt(Coordinates& /* inout */ aWhere, const char * aValu
 			auto& line = mLines[aWhere.mLine];
 			auto d = UTF8CharLength(*aValue);
 			while (d-- > 0 && *aValue != '\0')
-				line.insert(line.begin() + cindex++, Glyph(*aValue, PaletteIndex::Default));
-
-			if (*aValue == '\t')
-				aWhere.mColumn += mTabSize;
-			else
-				++aWhere.mColumn;
-
-			++aValue;
+				line.insert(line.begin() + cindex++, Glyph(*aValue++, PaletteIndex::Default));
+			aWhere.mColumn = GetCharacterColumn(aWhere.mLine, cindex);
 		}
 
 		mTextChanged = true;
