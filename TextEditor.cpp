a9830ee8fc640f3efa56ba115b405457933e4789
Aiekick <aiekick@funparadigm.com>
2020-08-04 19:00:20 +0200

    fix func isascii issue on some os

--- TextEditor.cpp
+++ TextEditor.cpp
@@ -9,6 +9,10 @@
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include "imgui.h" // for imGui::GetCurrentWindow()
 
+#ifndef isascii
+#define isascii(a) ((unsigned)(a) < 128)
+#endif
+
 // TODO
 // - multiline comments vs single-line: latter is blocking start of a ML
 
