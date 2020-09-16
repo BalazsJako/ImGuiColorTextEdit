226c3230de51c43b92f62ab8d6e150c63fbb77f9
Aiekick <aiekick@funparadigm.com>
2020-09-16 22:21:39 +0200

    fix when a know token is ont the first line first column, we have the tooltip when we are outside fo the editor

--- TextEditor.cpp
+++ TextEditor.cpp
@@ -1085,7 +1085,7 @@ void TextEditor::Render()
 		}
 
 		// Draw a tooltip on known identifiers/preprocessor symbols
-		if (ImGui::IsMousePosValid())
+		if (ImGui::IsMousePosValid() && ImGui::IsWindowHovered())
 		{
 			auto id = GetWordAt(ScreenPosToCoordinates(ImGui::GetMousePos()));
 			if (!id.empty())
